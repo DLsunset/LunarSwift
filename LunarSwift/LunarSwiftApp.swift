dff04065c91f63b01c35d30494c660c9b27da168
Dong Lei <donglei1115@126.com>
2026-03-10 12:26:02 +0800

    Initial Commit

---
+++ LunarSwift/LunarSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  LunarSwiftApp.swift
+//  LunarSwift
+//
+//  Created by Dong L on 2026/3/10.
+//
+
+import SwiftUI
+
+@main
+struct LunarSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
